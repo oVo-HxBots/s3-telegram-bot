c2a53bf87af5828d0275581d76c22f61a38b0dfa
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-16 13:08:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Yes! ✅");
-    return;
+      bot.sendMessage(msg.chat.id, "Yes! ✅");
+      return;
     }
-
-
+     
+       
 bot.on("message", async (msg) => {
-
+     
     if (!msg.document.mime_type.startsWith("image/")) {
       bot.sendMessage(msg.chat.id, "Unsupported file type. Try again!");
       return;
